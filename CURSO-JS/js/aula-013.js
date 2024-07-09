b1946e77f55f033a42b63a024f5ee4a0373bf742
Guilhermepereirafonseca <guilhermepereirafonseca305@gmail.com>
2024-07-09 10:45:05 -0300

    colocando arquivo que estava fora da pasta

---
+++ CURSO-JS/js/aula-013.js
@@ -0,0 +1,13 @@
+// OBJETOS
+
+let pessoa = {
+    idade: 18,
+    estudar(){
+        console.log("Estudando")
+    },
+    altura: 180
+}
+
+console.log(pessoa.idade)
+console.log(pessoa.altura)
+pessoa.estudar() // função
